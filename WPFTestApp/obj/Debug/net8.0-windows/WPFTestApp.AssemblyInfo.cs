1626c90456c197e515e66230fae49784a6f15647
marsomate <122260869+marsomate@users.noreply.github.com>
2024-04-19 11:35:47 +0200

    juhu

--- WPFTestApp/obj/Debug/net8.0-windows/WPFTestApp.AssemblyInfo.cs
+++ WPFTestApp/obj/Debug/net8.0-windows/WPFTestApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPFTestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09bac1a26d2860e523d186eaadd3a3b54e13c144")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPFTestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPFTestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
