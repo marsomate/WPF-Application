7dc98fd7486a0a377eec57d1b0f6e557a10def32
Laci1114 <placi11.14@gmail.com>
2024-04-22 11:15:08 +0200

    GameEngine osztály, kezdetleges mozgatás, még hibás

--- PicturePuzzle/PicturePuzzle/GameEngine.cs
+++ PicturePuzzle/PicturePuzzle/GameEngine.cs
@@ -1,20 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace PicturePuzzle
 {
     public class GameEngine
     {
-        public int[,] solution = new int[,]
+        public int[,] solution;
+        public int[,] currentPos;
+
+        public GameEngine()
         {
-            { 1, 2, 3 },
-            { 4, 5, 6 },
-            { 7, 8, -1 }
-        };
+            solution = new int[,]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, -1 }
+            };
 
+            currentPos = new int[,]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, -1 }
+            };
+        }
 
+        public void TryMove(Button clickedButton, Action<Button> swapButtons)
+        {
+            int buttonId = Convert.ToInt32(clickedButton.Name.Remove(0, "button".Length));
+            (int buttonPos_x, int buttonPos_y) = FindButtonPosition(buttonId);
+
+            if (IsNextToEmpty(buttonPos_x, buttonPos_y))
+            {
+                swapButtons(clickedButton);
+                UpdateArrayPosition(buttonPos_x, buttonPos_y);
+            }
+        }
+
+        private (int, int) FindButtonPosition(int buttonId)
+        {
+            (int, int) buttonPos = (0, 0);
+
+            for (int i = 0; i < currentPos.GetLength(0); i++)
+            {
+                for (int j = 0; j < currentPos.GetLength(1); j++)
+                {
+                    if (currentPos[i, j] == buttonId)
+                    { buttonPos = (i, j); }
+                }
+            }
+
+            return buttonPos;
+        }
+
+        private bool IsNextToEmpty(int buttonPos_x, int buttonPos_y)
+        {
+            if ((buttonPos_x != 0) && (currentPos[buttonPos_x - 1, buttonPos_y] == -1))
+            { return true; }
+            else if ((buttonPos_x != 2) && (currentPos[buttonPos_x + 1, buttonPos_y] == -1))
+            { return true; }
+            else if ((buttonPos_y != 0) && (currentPos[buttonPos_x, buttonPos_y - 1] == -1))
+            { return true; }
+            else if ((buttonPos_y != 2) && (currentPos[buttonPos_x, buttonPos_y + 1] == -1))
+            { return true; }
+
+            return false;
+        }
+
+        private void UpdateArrayPosition(int buttonPos_x, int buttonPos_y)
+        {
+            (int emptyPos_x, int emptyPos_y) = FindButtonPosition(-1);
+            Debug.Write($"Empty {emptyPos_x}, {emptyPos_y}");
+            currentPos[emptyPos_x, emptyPos_y] = currentPos[buttonPos_x, buttonPos_x];
+            currentPos[buttonPos_x, buttonPos_x] = -1;
+
+            Debug.Write('\n');
+            for (int i = 0; i < currentPos.GetLength(0); i++)
+            {
+                for (int j = 0; j < currentPos.GetLength(1); j++)
+                {
+                    Debug.Write(currentPos[i, j]);
+                }
+                Debug.Write('\n');
+            }
+        }
     }
 }
--- PicturePuzzle/PicturePuzzle/MainWindow.xaml.cs
+++ PicturePuzzle/PicturePuzzle/MainWindow.xaml.cs
@@ -20,16 +20,62 @@ namespace PicturePuzzle
     /// </summary>
     public partial class MainWindow : Window
     {
+        GameEngine gameEngine;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            gameEngine = new GameEngine();
+        }
+
+
+        public void SwapButtons(Button button)
+        {
+            var empty_pos = empty.Margin;
+            empty.Margin = button.Margin;
+            button.Margin = empty_pos;
+        }
+
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            gameEngine.TryMove(button1, SwapButtons);
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            gameEngine.TryMove(button2, SwapButtons);
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            gameEngine.TryMove(button3, SwapButtons);
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            gameEngine.TryMove(button4, SwapButtons);
+        }
+
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            gameEngine.TryMove(button5, SwapButtons);
         }
 
         private void button6_Click(object sender, RoutedEventArgs e)
         {
-            var empty_pos = empty.Margin;
-            empty.Margin = button6.Margin;
-            button6.Margin = empty_pos;
+            gameEngine.TryMove(button6, SwapButtons);
+        }
+
+        private void button7_Click(object sender, RoutedEventArgs e)
+        {
+            gameEngine.TryMove(button7, SwapButtons);
+        }
+
+        private void button8_Click(object sender, RoutedEventArgs e)
+        {
+            gameEngine.TryMove(button8, SwapButtons);
         }
     }
 }
