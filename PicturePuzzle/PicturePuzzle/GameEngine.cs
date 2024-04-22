6ec818322ac9c68f805db790cbdb1406f85d90e4
Laci1114 <placi11.14@gmail.com>
2024-04-22 10:20:31 +0200

    Új PicturePuzzle projekt

---
+++ PicturePuzzle/PicturePuzzle/GameEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicturePuzzle
+{
+    public class GameEngine
+    {
+        public int[,] solution = new int[,]
+        {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, -1 }
+        };
+
+
+    }
+}
