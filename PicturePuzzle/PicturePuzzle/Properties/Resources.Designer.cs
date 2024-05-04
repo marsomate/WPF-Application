60d57780b855a3726b10cb84c373669fac603079
Laci1114 <placi11.14@gmail.com>
2024-05-04 12:44:54 +0200

    Próbálkozások a relatív kép hozzáféréssel. Még nem működik

--- PicturePuzzle/PicturePuzzle/Properties/Resources.Designer.cs
+++ PicturePuzzle/PicturePuzzle/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace PicturePuzzle.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap _1 {
+            get {
+                object obj = ResourceManager.GetObject("_1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap background {
             get {
                 object obj = ResourceManager.GetObject("background", resourceCulture);
